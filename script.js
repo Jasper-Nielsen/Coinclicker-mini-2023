8db754b17a315232d8af1466a77a88d552794e0b
Jasper Nielsen <jasper.nielsen@hotmail.com>
2023-02-20 19:46:39 +0100

    can click and coin disappears then animation resets

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 window.addEventListener("load", start);
 
 function start() {
-    // document.querySelector("#coin1_container").classList.add("falling");
+  // document.querySelector("#coin1_container").classList.add("falling");
   document.querySelector("#coin1_container").addEventListener("click", onClick);
-  document.querySelector("#coin1_container")
+  document
+    .querySelector("#coin1_container")
     .addEventListener("animationend", reset);
 }
 
 function onClick() {
-    // document
-    //   .querySelector("#coin1_container")
-    //   .removeEventListener("click", onClick);
+  // document
+  //   .querySelector("#coin1_container")
+  //   .removeEventListener("click", onClick);
   document.querySelector("#coin1_container").classList.add("paused");
   document.querySelector("#coin1_sprite").classList.add("zoom_out");
 }
 
 function reset() {
-    document.querySelector("#coin1_container").classList.remove("paused");
-    document.querySelector("#coin1_sprite").classList.remove("zoom_out");
+  // below removes paused and zoom_out so you can see coin again
+  document.querySelector("#coin1_container").classList.remove("paused");
+  document.querySelector("#coin1_sprite").classList.remove("zoom_out");
+
+  // below resets animation.
+  document.querySelector("#coin1_container").classList.remove("falling");
+  document.querySelector("#coin1_container").offsetLeft;
+  document.querySelector("#coin1_container").classList.add("falling");
 }
